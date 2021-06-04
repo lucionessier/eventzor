d615d0f8ab89c8cc6338d8488d4c4e81dd276c65
LucioNessier <nessier.lucio@gmail.com>
2021-06-04 15:33:21 -0300

    Agregada funcionalidad de boton ver más

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 // Creamos un array con 20 objetos (distintas bandas), para que posteriormente puedan iterarse tanto en la search bar como en el resto del código
 
-
-
 const bandas = [
     
     {
@@ -191,118 +189,51 @@ for (let i = 0; i < bandas.length && i <= 3 ; i++) {
 
 }
 
+// Creo una variable "displayMore = ver más en ingles". La agarro con elementbyId y le agrego el eventlistener que va a hacer que vaya mostrando el resto de los eventos cada vez que le hago click al boton .
+
+let displayMore = document.getElementById("verMas").addEventListener("click", mostrarBandas);
 
 
-// //Preguntará, en este caso 3 veces, ya que cantidad es igual a 3, al usuario que ingrese bandas.
-// do {
+// Esta variable empieza en 4, ya que los 4 primeros elementos se muestran con el for previo de más arriba
+var posicion = 4;
+
+//La funcion del addeventListener que va creando el resto de las cards cada vez que el boton es apretado
+function mostrarBandas() { 
+
+    console.log('botonVerMas');
+
+    for(let i=posicion; i < (posicion +4); i++) {
+        let crearCard = document.querySelector('#proximosEventos');
     
-//     let ingresarBanda = prompt("Ingresa el nombre de una banda");
-//     bandas.push(ingresarBanda.toUpperCase());
+        crearCard.innerHTML += `
+        <div class="container-fluid col-md-3 justify-content-center align-content-center" style="margin-bottom: 2em">
+        <div class="card align-items-center">
+          <img src="${bandas[i].imgFile}" class="card-img-top" alt="...">
+          
+          <div class="card-body container row justify-content-center"></div>
+            <h5>${bandas[i].fechaBanda}</h5>
+            <h4 class="card-title">${bandas[i].nombreBanda}</h4>
+            <p class="descripcion-evento">${bandas[i].lugarBanda}</p>
+            <a href="#" class="d-flex boton-proximos-eventos btn btn-primary justify-content-center align-content-center" style="margin-bottom:1em;">Comprar Tickets</a>
+          </div>
+        </div>
+      </div>`;
 
+    }
 
-// } while(bandas.length != cantidad) 
+    posicion = posicion + 4;
 
-// // Concatenamos las bandas ingresadas
-// let bandasIngresadas = bandas.concat();
+    // Cuando posición llegue a 20, se ejecutará lo siguiente:
 
-// // Mostramos las bandas que ingresó
-// alert("Las bandas ingresadas en el buscador son: " + bandasIngresadas.join("\n"));
-
-// // Creamos un array de meses
-// let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio", "Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-
-
-// // Creamos una funcion que genere un mes random de los listados en el array anterior
-// function mesRandom () {
-//     return meses[Math.floor(Math.random() * meses.length)];
-// }
-
-
-// // Lo mismo pero que agarre una banda cualquiera de las ingresadas
-// function bandaRandom () {
-//     return bandas[Math.floor(Math.random() * bandas.length)];
-// }
-
-
-
-// // Con el class constructor, creo 3 objetos, cada uno con una region, ciudad y establecimiento distintos . Después los pongo en un array para que sea más fácil acceder 
-
-// class Lugar {
+    if(posicion === 20) {
+        // La visibilidad es hidden cuando llega al tope de los elementos (20)
+        document.getElementById('verMas').style.visibility = 'hidden';
+    } 
     
-//     constructor(region,ciudad,establecimiento) {
-//         this.region = region;
-//         this.ciudad = ciudad,
-//         this.establecimiento = establecimiento
-//     }
+    // Si no, sigue en visible para que pueda seguir clickeandose:
 
-//     mostrarLugar() {
-//         console.log(Lugar);
-//     }
+    else {
+        document.getElementById('verMas').style.visibility = 'visible';
+    }
 
-// }
-
-// const ciudad1 = new Lugar('America del Norte','Nueva York', 'Geekbar');
-
-// const ciudad2 = new Lugar('Europa','Londres','The Strokes');
-
-// const ciudad3 = new Lugar('Asia','China','CoderBar');
-
-// let ciudades = [ciudad1,ciudad2,ciudad3];
-
-// function lugarRandom () {
-//     return ciudades[Math.floor(Math.random() * ciudades.length)];
-// }
-
-
-// // Una variable que me seleccione la clase del div infoDisplay
-
-// let displayInfo = document.querySelector('.infoDisplay');
-
-// // Una variable que cree el elemento h2
-
-// let titulosInfo = document.createElement('h2');
-
-
-// // Template literals para que la display de la info aparezca en el div vacío en forma de h2:
-
-// titulosInfo.textContent = `
-// La banda ingresada ${bandaRandom()} tocará el mes ${mesRandom()} en ${lugarRandom().establecimiento} , ${lugarRandom().ciudad} 
-// `;
-
-// // AppendChild que haga el display en el div.
-// displayInfo.appendChild(titulosInfo);
-
-
-
-// // -------- DESAFÍO 9 ------
-
-
-// // Una variable que me seleccione la clase del div botonComprar
-
-// let displayCompra = document.querySelector('.botonComprar');
-
-// // Creo un botón con el innerHTML, con class incluida
-
-// displayCompra.innerHTML = `<button class="boton">COMPRAR TICKET</button>`;
-
-
-// // Creo una funcion que genere un precio random en un rango entre 1 y 400
-
-
-// function precioRandom () {
-//     return Math.floor((Math.random()*400) + 1)
-// }
-
-
-// // Creo el evento que va a ejecutarse onClick del botón 'Comprar Ticket'
-
-// displayCompra.addEventListener('click', mostrarPrecio);
-
-// function mostrarPrecio () {
-
-//     let infoCompra = document.querySelector('.displayPrecio');
-
-
-//     infoCompra.innerHTML = `<h3 style="color:#00e6b3; font-weight:bold;">El precio del ticket para la banda ${bandaRandom()} es de $${precioRandom()}</h3>`
-
-// }
+}
